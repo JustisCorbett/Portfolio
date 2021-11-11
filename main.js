6755c143d284d47271a89b6967080348934218b8
Justis Corbett <corbettjustis@gmail.com>
2021-11-10 17:20:53 -0700

    changed borders to shadows, button tab fix

--- main.js
+++ main.js
@@ -65,7 +65,6 @@ async function activateSection (evt) {
     let sectionName = evt.target.dataset.section;
     let box = document.getElementById("box-container");
     console.log(sectionName);
-    box.classList.add("shrink");
     
     for (let i = 0; i < sectionButtons.length; i++) {
         let el = sectionButtons[i];
@@ -76,6 +75,7 @@ async function activateSection (evt) {
             el.classList.remove("selected");
         }
     }
+    box.classList.add("shrink");
     await new Promise(r => setTimeout(r, 500));
     for (let i = 0; i < sections.length; i++) {
         let el = sections[i];
